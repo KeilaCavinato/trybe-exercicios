470367e6edd2ca2d13873350dfb13f887d7e560a
Keila <nanrenanda@gmail.com>
2021-10-14 18:23:57 -0300

    exercicios usando for

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/oito-script.js
@@ -0,0 +1,9 @@
+/*Utilizando for , crie um array q
+ue vá de 1 até 25 e imprima o resultado;*/
+
+let adiciona = [];
+
+for (let contador = 1; contador <= 25; contador++) {
+    adiciona.push(contador)
+}
+console.log(adiciona)
