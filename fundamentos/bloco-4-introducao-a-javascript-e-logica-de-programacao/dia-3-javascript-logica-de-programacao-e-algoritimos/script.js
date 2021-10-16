dfc1163a43715c0fb25f3685a4bbb547227fca59
Keila <nanrenanda@gmail.com>
2021-10-16 16:19:13 -0300

    exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/script.js
@@ -0,0 +1,22 @@
+/*1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+*/
+let numeroLinha = 5;
+let altura = 5;
+let asterisco = '*';
+let resultado = '';
+
+
+for (let i = 0; i <= numeroLinha; i+ 1) {
+    
+    console.log(resultado)
+    resultado = resultado + asterisco + altura;
+
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/tres-script.js
@@ -0,0 +1,18 @@
+/*3- Agora inverta o lado do triângulo. Por exemplo:
+n = 5
+
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+let numeroLinha = 5;
+let asterisco = '*';
+let resultado = '';
+
+for (i = 0; i <= numeroLinha; i += 1){
+console.log(resultado);
+resultado = resultado + asterisco
+}
