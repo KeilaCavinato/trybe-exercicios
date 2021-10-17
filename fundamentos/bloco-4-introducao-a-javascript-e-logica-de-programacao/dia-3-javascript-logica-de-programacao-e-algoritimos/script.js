0bfa6217805390a12617d899996ec3c3f2069dcb
Keila <nanrenanda@gmail.com>
2021-10-17 16:11:07 -0300

    atualizando branch

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/script.js
@@ -0,0 +1,20 @@
+/*1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+*/
+let numeroLinha = 5;
+let asterisco = '*';
+let resultado = '';
+
+
+for (let i = 0; i < numeroLinha; i+= 1) {
+    resultado = resultado + asterisco;
+    
+} 
+console.log(resultado)
